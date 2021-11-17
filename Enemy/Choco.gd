4c00336d258f043387cfba024c248bf630e251f6
Misty Becker <mistyb01@uw.edu>
2021-11-17 13:23:07 -0800

    enemies change direction

--- Enemy/Choco.gd
+++ Enemy/Choco.gd
@@ -1,28 +1,40 @@
-extends RigidBody2D
+extends Area2D
 
 var knockback = Vector2.ZERO
 var chocoItem = preload("res://Objects/Items/ChocoItem.tscn")
 var chocoWrapper = preload("res://Objects/Items/ChocoWrapper.tscn")
+
+var direction = Vector2.ZERO
+
+var max_point = Vector2.ZERO
+var min_point = Vector2.ZERO
 
 export var min_speed = 50
 export var max_speed = 150
 
 onready var stats = $Stats
 
-
+func _ready():
+	direction = position.direction_to(
+				lerp(max_point, min_point, randf()))
+	
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	#knockback = move_and_slide(knockback)
+	position += direction * delta * max_speed
+	
+	if position.x > max_point.x:
+		queue_free()
 
 func item_drop():
 	var food = chocoItem.instance()
 	var trash = chocoWrapper.instance()
 
 	food.global_position = Vector2(global_position.x - 50, global_position.y)
-	get_tree().get_root().add_child(food)
+	get_tree().get_root().call_deferred("add_child", food)
 	
 	trash.global_position = Vector2(global_position.x + 50, global_position.y)
-	get_tree().get_root().add_child(trash)
+	get_tree().get_root().call_deferred("add_child", trash)
 	
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
@@ -33,3 +45,7 @@ func _on_Stats_no_health():
 	queue_free()
 	item_drop()
 
+func _on_ChangeDirection_timeout():
+	direction = position.direction_to(
+				lerp(max_point, min_point, randf()))
+	
--- World/World.gd
+++ World/World.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
-export (PackedScene) var Enemy
+export (Array, PackedScene) var Enemy
 
 var items = 0
+onready var max_point = $max_point.position
+onready var min_point = $min_point.position
+
 
 func _ready():
 	$SpawnTimer.start()
@@ -11,11 +14,16 @@ func _on_SpawnTimer_timeout():
 	# Choose a random location on Path2D.
 	$SpawnPath/SpawnPoint.offset = randi()
 	
-	var enemy = Enemy.instance()
-	add_child(enemy)
+	var enemy = Enemy[int(randf() * Enemy.size())].instance()
 	
 	enemy.position = $SpawnPath/SpawnPoint.position
-	var direction = $SpawnPath/SpawnPoint.rotation + PI / 2
-	enemy.linear_velocity = Vector2(rand_range(enemy.min_speed, enemy.max_speed), 0)
-	enemy.linear_velocity = enemy.linear_velocity.rotated(direction)
+	enemy.max_point = max_point
+	enemy.min_point = min_point
+	
+	add_child(enemy)
+	
+
+#	var direction = $SpawnPath/SpawnPoint.rotation + PI / 2
+#	enemy.linear_velocity = Vector2(rand_range(enemy.min_speed, enemy.max_speed), 0)
+#	enemy.linear_velocity = enemy.linear_velocity.rotated(direction)
 
