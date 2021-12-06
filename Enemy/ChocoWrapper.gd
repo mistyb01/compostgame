c1cab9521099ce77e37f4fcd24d37dc6bc32b259
Misty Becker <mistyb01@uw.edu>
2021-12-06 12:55:32 -0800

    added burger and choco-wrapper enemy

---
+++ Enemy/ChocoWrapper.gd
@@ -0,0 +1,14 @@
+extends "res://Enemy/TrashEnemyBase.gd"
+
+
+onready var stats = $Stats
+
+func _on_Hurtbox_area_entered(area):
+	stats.health -= area.damage
+	knockback = area.knockback_vector * 50
+
+func _on_Stats_no_health():
+	queue_free()
+	pop_up()
+	Global.enemyCount -= 1
+
